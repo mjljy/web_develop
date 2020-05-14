6cbdb9406a587a48f793cfbbfeae6d67927205fb
mjljy <mjljy0379@163.com>
2020-05-14 16:19:56 +0800

    0514-----------------------------

--- chapter3/section1/app_with_custom_converter.py
+++ chapter3/section1/app_with_custom_converter.py
@@ -3,27 +3,20 @@ import urllib
 
 from flask import Flask
 from werkzeug.routing import BaseConverter
-
 app = Flask(__name__)
-
-
 class ListConverter(BaseConverter):
-
     def __init__(self, url_map, separator='+'):
         super(ListConverter, self).__init__(url_map)
         self.separator = urllib.unquote(separator)
 
     def to_python(self, value):
         return value.split(self.separator)
-
     def to_url(self, values):
-        return self.separator.join(BaseConverter.to_url(self, value)
-                                   for value in values)
-
+        return self.separator.join(BaseConverter.to_url(self, value)for value in values)
 
 app.url_map.converters['list'] = ListConverter
-
-
+# <converter:variable_name>
+# 列表 类型 page_names
 @app.route('/list1/<list:page_names>/')
 def list1(page_names):
     return 'Separator: {} {}'.format('+', page_names)
